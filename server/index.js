090c6cd48b8e26f2918fa4e19e2d75b408f8d2da
miguelgnz <80795535+miguelgnz@users.noreply.github.com>
2021-04-28 16:08:18 -0600

    Connecting to DB & structuring form

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app =  express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended : true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended : true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:1234@cluster0.yrpzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log('Server running on port: 5000')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
